6d92989ed0e82c41bb77a22f5e450c4a9b08143f
cardoso <matheus@cardo.so>
2018-07-15 20:04:43 -0300

    ✨ feat: add Message model

--- Package.swift
+++ Package.swift
@@ -20,7 +20,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "SwiftChatCore",
-            dependencies: []),
+	    path: "Sources"),
         .testTarget(
             name: "SwiftChatCoreTests",
             dependencies: ["SwiftChatCore"]),
--- Sources/SwiftChatCore/SwiftChatCore.swift
+++
@@ -1,3 +0,0 @@
-struct SwiftChatCore {
-    var text = "Hello, World!"
-}
